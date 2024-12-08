94f52aa12d500ebb45822da6c5762d6973794f5a
Anastasiia Prokofieva <prokofieva.nastia42342@gmail.com>
2024-12-08 03:51:40 +0000

    Persistent storage is added to the project (CSV and Json files in use)

--- Student_data_project.py
+++ Student_data_project.py
@@ -1,26 +1,65 @@
+import csv
+import json
+from pathlib import Path
+
 # Simulated storage
 # ==================================================================================================
-students = {
-    1: {
-        'id': 1,
-        'name': 'Ian Taylor',
-        'marks': [4, 3, 5, 5, 4, 3, 4, 4, 2],
-        'info': 'Maile, 20 y.o.'
-    },
-    2: {
-        'id': 2,
-        'name': 'Amy Smith',
-        'marks': [5, 5, 4, 5, 5, 4, 3, 5, 5],
-        'info': 'Female, 21 y.o.'
-    }
-}
+csv_file = Path("/Users/anastasiiaprokofieva/PycharmProjects/Python_PRO")
+files_dir = Path("/Users/anastasiiaprokofieva/PycharmProjects/Python_PRO")
+csv_storage = 'students.csv'
+storage_file = 'students.json'
 
-last_setup_id = 2
+
+class StudentsRepository:
+
+    def __init__(self) -> None:
+        self.students = {}
+        self.students.update(self.load_csv())
+
+    @staticmethod
+    def read_json(self) -> dict:
+        with open(files_dir / storage_file) as file:
+            return json.load(file)
+
+    @staticmethod
+    def write_json(data: dict) -> None:
+        with open(files_dir / storage_file, mode='w') as file:
+            json.dump(data, file)
+
+    @staticmethod
+    def load_csv() -> dict:
+        students = {}
+        with open(csv_file / csv_storage, mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                students[row['id']] = {
+                    'name': row['name'],
+                    'marks': list(map(int, row['marks'].split(',')))
+                }
+        return students
+
+
+    @staticmethod
+    def write_csv(data: dict) -> None:
+        with open(csv_file / csv_storage, mode='w') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'name', 'marks'])
+            writer.writeheader()
+            for student_id, student_data in data.items():
+                writer.writerow({
+                    'id': student_id,
+                    'name': student_data['name'],
+                    'marks': ','.join(map(str, student_data['marks']))
+                })
+
+    def update_students(self) -> None:
+        self.write_json(self.students)
+        self.write_csv(self.students)
 
 
 def students_show():
-    for id_, student in students.items():
-        print(f'[{id_}] {student['name']}. Marks: {student['marks']}')
+
+    for id_, student in StudentsRepository().students.items():
+        print(f"[{id_}] {student['name']}. Marks: {student['marks']}")
 
 
 def student_details(student: dict) -> None:
@@ -31,42 +70,54 @@ def student_details(student: dict) -> None:
 
 
 def students_add(student: dict) -> dict | None:
-    global last_setup_id
+    storage = StudentsRepository()
 
     if len(student) != 2:
         return None
     if not student.get('name') or not student.get('marks'):
         return None
-    else:
-        last_setup_id += 1
-        students[last_setup_id] = student
 
+    if storage.students:
+        current_id = list(map(int, storage.students.keys()))
+        new_id = str(max(current_id) + 1)
+    else:
+        new_id = '1'
+
+    storage.students[new_id] = student
+    storage.update_students()
     return student
 
 
 def students_retrieve(id_: int) -> dict | None:
-    return students.get(id_)
+    storage = StudentsRepository()
+    return storage.students.get(str(id_))
 
 
 def students_remove(id_: int):
+    storage = StudentsRepository()
     if students_retrieve(id_):
-        del students[id_]
+        del storage.students[str(id_)]
+        storage.update_students()
         print(f"Student with id '{id_}' is removed from the list")
     else:
         print(f"There is no requested student with id '{id_}' in the list")
 
 
 def students_add_mark(id_: int):
+    storage = StudentsRepository()
     additional_mark = input('Please type your mark to add: ')
-    if id_ in students:
-        students[id_]['marks'].append(int(additional_mark))
+    if id_ in storage.students:
+        storage.students[str(id_)]['marks'].append(int(additional_mark))
+        storage.update_students()
         print("Mark was successfully added!")
     else:
         print(f"There is no requested student with id '{id_}' in the list")
 
 
 def students_update(id_: int, payload: dict) -> dict:
-    students[id_] = payload
+    storage = StudentsRepository()
+    storage.students[str(id_)] = payload
+    storage.update_students()
     return payload
 
 
@@ -94,12 +145,14 @@ def parse(data: str) -> tuple[str, list[int]]:
 
 
 def students_partial_update(id_: int, part_payload: dict) -> dict:
-    students[id_] = part_payload
+    storage = StudentsRepository()
+    storage.students[str(id_)] = part_payload
+    storage.update_students()
     return part_payload
 
 
 def student_part_payload(id_: int) -> dict:
-
+    storage = StudentsRepository()
     prompt_part = input(
         "Type the part of student's payload you would like to change.\n"
         "(Example for the name update: John Doe\n"
@@ -114,12 +167,12 @@ def student_part_payload(id_: int) -> dict:
         items_part = items_part1.split(' ')
         update_part = ''.join(items_part)
 
-        if id_ in students and update_part.isalpha():
+        if id_ in storage.students and update_part.isalpha():
             new_name = ' '.join(items_part)
-            return {'name': new_name, 'marks': students[id_]['marks']}
-        elif id_ in students and update_part.isdigit():
+            return {'name': new_name, 'marks': storage.students[id_]['marks']}
+        elif id_ in storage.students and update_part.isdigit():
             new_marks = list(update_part)
-            return {'name': students[id_]['name'], 'marks': new_marks}
+            return {'name': storage.students[id_]['name'], 'marks': new_marks}
         else:
             raise Exception('Incorrect formating')
 
@@ -163,9 +216,10 @@ def handle_user_commands(command: str):
             students_remove(id_)
 
     elif command == 'update':
+        storage = StudentsRepository()
         update_id = input("Type student's id to update: ")
-        id_ = int(update_id)
-        if id_ not in students:
+        id_ = str(update_id)
+        if id_ not in storage.students:
             print(f"There is not student with id '{update_id}'")
         else:
             if data := student_part_payload(id_):
@@ -187,9 +241,10 @@ def handle_user_commands(command: str):
                 print(f"New student '{student['name']}' is created")
 
     elif command == 'add mark':
+        storage = StudentsRepository()
         mark_id = input("Type student's id to add mark: ")
-        id_ = int(mark_id)
-        if id_ not in students:
+        id_ = str(mark_id)
+        if id_ not in storage.students:
             print(f"There is not student with id '{mark_id}'")
         else:
             students_add_mark(id_)
